39682a1d54bb1d83067b8f1f766e341911b1a129
Nunu-DMG09 <davidmesta09@gmail.com>
2025-09-26 10:57:52 -0500

    Fix namespace for AdministradoresModel in AuthFilter

--- backend/app/Filters/AuthFilter.php
+++ backend/app/Filters/AuthFilter.php
@@ -5,7 +5,7 @@ namespace App\Filters;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
-use App\Models\Denuncias\AdministradoresModel;
+use App\Models\denuncias_corrupcion\denuncias\AdministradoresModel;
 
 helper("cookie");
 helper("jwt");
